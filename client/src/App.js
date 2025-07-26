09caffc4aca3b251d787e5aeabcf471cddd46449
Saili Zulu <chickiechicks.biz@gmail.com>
2025-07-26 19:32:00 +0300

    Set up React app with Tailwind and initial files

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-3xl font-bold text-blue-600">Hello, Chickie Chicks!</h1>
+      <p>Welcome to your React app.</p>
+    </div>
+  );
+}
+
+export default App;
